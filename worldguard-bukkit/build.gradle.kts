d9d315c8f9a8758919818be79b53a3ec77c3d5e7
CoolLoong <wingdon@foxmail.com>
2025-07-17 22:09:00 +0800

    feat: adapt to paper-plugin

--- worldguard-bukkit/build.gradle.kts
+++ worldguard-bukkit/build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
 tasks.named<Copy>("processResources") {
     val internalVersion = project.ext["internalVersion"]
     inputs.property("internalVersion", internalVersion)
-    filesMatching("plugin.yml") {
+    filesMatching("paper-plugin.yml") {
         expand("internalVersion" to internalVersion)
     }
 }
